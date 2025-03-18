d0e5e1578be6373d00e7282d43f5817fb9c1634f
Jorge M. Carpanetti <carpanetti@naver.com>
2025-03-18 12:28:04 -0700

    Update app.js
    .

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    if (nombre === '') {
+        alert('Por favor, inserte un nombre.');
+        return;
+    }
+
+    amigos.push(nombre);
+    input.value = '';
+}
